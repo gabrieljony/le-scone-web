37d8e6bebc7558166f0133a4f7c455557ef75582
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-04-08 22:04:24 -0300

    le scone v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { HomeComponent } from './pages/home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
